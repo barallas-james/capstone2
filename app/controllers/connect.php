68cab4a591b07fb8d47054438b3224845845ffec
James Barallas <barallas.james@gmail.com>
2019-01-21 11:44:45 +0800

    changed connection

--- app/controllers/connect.php
+++ app/controllers/connect.php
@@ -1,10 +1,14 @@
 <?php 
 
-$host = 'localhost';
-$username = 'root';
-$password = '';
-$dbname = 'e-com';
+// $host = 'localhost';
+// $username = 'root';
+// $password = '';
+// $dbname = 'e-com';
 
+$host = "db4free.net";
+$username = "barallas_ecom";
+$password = "James4523";
+$dbname = "ecom_capstone2";
 
 $conn = mysqli_connect($host, $username, $password, $dbname);
 
